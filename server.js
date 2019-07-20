2f1f9a82a4ff4f32ca57e60178ef094caae24a80
Viriya <viriyaluckyo@gmail.com>
2019-07-21 00:23:57 +0700

    tenth

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var app = express();
 const path = require('path');
-app.use(express.static('/dist')); // myApp will be the same folder name.
+app.use(express.static('../../dist')); // myApp will be the same folder name.
 
 app.listen(process.env.PORT || 8080);
 app.get('/*', function (req, res) {
-  res.sendFile(path.join('/dist/index.html'));
-  console.log(__dirname + '/dist/index.html');
+  res.sendFile(path.join('../../dist/index.html'));
+  console.log('../../dist/index.html');
 });
 console.log("MyProject Server is Listening on port 8080");
 
